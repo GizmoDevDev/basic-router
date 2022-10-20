ac06361a9e7dbfa6f9a8a2dac2e8268edb1a558a
Sergey Mazaev <mazai.segr@gmail.com>
2022-10-20 12:07:49 +0530

    simple routes nextjs

--- pages/deploy.tsx
+++ pages/deploy.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const Deploy: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -14,37 +14,18 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a> deploy
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          <code className={styles.code}>pages/deploy.tsx</code>
         </p>
+
         <div className={styles.grid}>
-          <a href="/deploy" className={styles.card}>Deploy</a>
-          <a href="/documentation" className={styles.card}>Documentation</a>
+          <a href="/" className={styles.card}>Back</a>
         </div>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
@@ -73,4 +54,4 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Deploy
